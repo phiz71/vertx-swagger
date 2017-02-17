e2791d3a17817640f56401691c0868b62a9bd4bd
Florent C <florent.chamfroy@gmail.Com>
2017-02-17 10:42:23 +0100

    remove unused import

--- modules/vertx-swagger-codegen/src/main/java/io/vertx/ext/swagger/codegen/JavaVertXServerGenerator.java
+++ modules/vertx-swagger-codegen/src/main/java/io/vertx/ext/swagger/codegen/JavaVertXServerGenerator.java
@@ -8,7 +8,6 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.apache.commons.lang3.BooleanUtils;
-import org.apache.commons.lang3.StringUtils;
 
 import io.swagger.codegen.CodegenConfig;
 import io.swagger.codegen.CodegenModel;
