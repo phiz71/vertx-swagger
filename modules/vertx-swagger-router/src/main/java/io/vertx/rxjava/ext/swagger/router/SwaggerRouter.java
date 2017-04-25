b8097c5873d798d9abd90c3acd5608552f70716c
Vincent von Rotz <vincent.vonrotz@gmail.com>
2017-04-25 14:41:11 +0200

    Fixed namespace issue

--- modules/vertx-swagger-router/src/main/java/io/vertx/rxjava/ext/swagger/router/SwaggerRouter.java
+++ modules/vertx-swagger-router/src/main/java/io/vertx/rxjava/ext/swagger/router/SwaggerRouter.java
@@ -3,8 +3,8 @@ package io.vertx.rxjava.ext.swagger.router;
 import io.swagger.models.Swagger;
 
 import io.vertx.core.eventbus.DeliveryOptions;
-import io.vertx.ext.swagger.router.DefaultServiceIdResolver;
-import io.vertx.ext.swagger.router.ServiceIdResolver;
+import com.github.phiz71.vertx.swagger.router.DefaultServiceIdResolver;
+import com.github.phiz71.vertx.swagger.router.ServiceIdResolver;
 import io.vertx.rxjava.core.eventbus.EventBus;
 import io.vertx.rxjava.ext.web.Router;
 import io.vertx.rxjava.ext.web.RoutingContext;
@@ -27,7 +27,7 @@ public class SwaggerRouter {
 
         Function<io.vertx.ext.web.RoutingContext, DeliveryOptions> configureMessageDelegate = rc -> configureMessage.apply(new RoutingContext(rc));
 
-        return new Router(io.vertx.ext.swagger.router.SwaggerRouter.swaggerRouter(baseRouterDelegate, swagger, eventBusDelegate, serviceIdResolver, configureMessageDelegate));
+        return new Router(com.github.phiz71.vertx.swagger.router.SwaggerRouter.swaggerRouter(baseRouterDelegate, swagger, eventBusDelegate, serviceIdResolver, configureMessageDelegate));
     }
 
 }
