c1794a892bffe8f74de5808f08b77153952a7bfb
Florent Chamfroy <florent.chamfroy@gmail.Com>
2017-07-02 16:10:08 +0200

    fixing body parameter extractor

--- modules/vertx-swagger-router/src/main/java/com/github/phiz71/vertx/swagger/router/extractors/BodyParameterExtractor.java
+++ modules/vertx-swagger-router/src/main/java/com/github/phiz71/vertx/swagger/router/extractors/BodyParameterExtractor.java
@@ -1,16 +1,30 @@
 package com.github.phiz71.vertx.swagger.router.extractors;
 
+import io.swagger.models.ArrayModel;
 import io.swagger.models.parameters.BodyParameter;
 import io.swagger.models.parameters.Parameter;
+import io.vertx.core.json.DecodeException;
 import io.vertx.ext.web.RoutingContext;
 
 public class BodyParameterExtractor implements ParameterExtractor {
     @Override
     public Object extract(String name, Parameter parameter, RoutingContext context) {
         BodyParameter bodyParam = (BodyParameter) parameter;
-        if ("".equals(context.getBodyAsString()) && bodyParam.getRequired()) {
-            throw new IllegalArgumentException("Missing required parameter: " + name);
+        if ("".equals(context.getBodyAsString())) {
+            if (bodyParam.getRequired())
+                throw new IllegalArgumentException("Missing required parameter: " + name);
+            else
+                return null;
         }
-        return context.getBodyAsString();
-    }
+
+        try {
+            if(bodyParam.getSchema() instanceof ArrayModel) {
+                return context.getBodyAsJsonArray();
+            } else {
+                return context.getBodyAsJson();
+            }
+        } catch (DecodeException e) {
+            return context.getBodyAsString();  
+        }
+    }        
 }
