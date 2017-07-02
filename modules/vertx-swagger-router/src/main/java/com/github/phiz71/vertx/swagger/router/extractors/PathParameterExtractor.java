e44a69bceeaf472c58cbd6e6b358a92705a7e0d4
Florent Chamfroy <florent.chamfroy@gmail.Com>
2017-07-02 16:17:21 +0200

    Simplify PathParameterExtractor
    According to Swagger Specification, path parameters are always required.
    And as the PathParameterExtractor is activated only if the request
    contains a path parameter, these tests are not needed

--- modules/vertx-swagger-router/src/main/java/com/github/phiz71/vertx/swagger/router/extractors/PathParameterExtractor.java
+++ modules/vertx-swagger-router/src/main/java/com/github/phiz71/vertx/swagger/router/extractors/PathParameterExtractor.java
@@ -1,18 +1,11 @@
 package com.github.phiz71.vertx.swagger.router.extractors;
 
 import io.swagger.models.parameters.Parameter;
-import io.swagger.models.parameters.PathParameter;
-import io.vertx.core.MultiMap;
 import io.vertx.ext.web.RoutingContext;
 
 public class PathParameterExtractor implements ParameterExtractor {
     @Override
     public Object extract(String name, Parameter parameter, RoutingContext context) {
-        PathParameter pathParam = (PathParameter) parameter;
-        MultiMap params = context.request().params();
-        if (!params.contains(name) && pathParam.getRequired()) {
-            throw new IllegalArgumentException("Missing required parameter: " + name);
-        }
-        return params.get(name);
+        return context.request().params().get(name);
     }
 }
