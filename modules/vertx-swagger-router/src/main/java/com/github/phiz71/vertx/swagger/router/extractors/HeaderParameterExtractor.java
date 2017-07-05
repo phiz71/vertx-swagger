47a856ea4f4f226e3fd501d8c9c2ee864c8cd253
Florent Chamfroy <florent.chamfroy@gmail.Com>
2017-07-05 20:55:58 +0200

    array of header need specific treatment

--- modules/vertx-swagger-router/src/main/java/com/github/phiz71/vertx/swagger/router/extractors/HeaderParameterExtractor.java
+++ modules/vertx-swagger-router/src/main/java/com/github/phiz71/vertx/swagger/router/extractors/HeaderParameterExtractor.java
@@ -1,11 +1,16 @@
 package com.github.phiz71.vertx.swagger.router.extractors;
 
+import io.swagger.models.parameters.HeaderParameter;
 import io.swagger.models.parameters.Parameter;
 import io.vertx.ext.web.RoutingContext;
 
 public class HeaderParameterExtractor extends AbstractSerializableParameterExtractor implements ParameterExtractor {
     @Override
     public Object extract(String name, Parameter parameter, RoutingContext context) {
+        HeaderParameter headerParam = (HeaderParameter) parameter;
+        if ("array".equals(headerParam.getType())) {
+            return context.request().headers().getAll(name);
+        }
         return this.extract(name, parameter, context.request().headers());
     }
 }
