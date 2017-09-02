8da539b85dcbf237274e399ed73346a9d21983ac
Florent Chamfroy <florent.chamfroy@gmail.Com>
2017-09-02 16:30:26 +0200

    Fixing #74
    rewirte enumOuterClass based on modelEnum in Java swagger-codegen
    templates.
    
    https://github.com/swagger-api/swagger-codegen/blob/master/modules/swagger-codegen/src/main/resources/Java/modelEnum.mustache

---
+++ sample/petstore-vertx-rx-server/src/main/java/io/swagger/server/api/model/ErrorCode.java
@@ -0,0 +1,45 @@
+package io.swagger.server.api.model;
+
+import java.util.Objects;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+/**
+ * Gets or Sets ErrorCode
+ */
+public enum ErrorCode {
+  
+  REQUIRED_RESOURCE_ID_NOT_SET("REQUIRED_RESOURCE_ID_NOT_SET"),
+  
+  REQUIRED_RESOURCE_NOT_FOUND("REQUIRED_RESOURCE_NOT_FOUND"),
+  
+  ALREADY_EXISTS("ALREADY_EXISTS"),
+  
+  SOMETHING_ELSE("SOMETHING_ELSE");
+
+  private String value;
+
+  ErrorCode(String value) {
+    this.value = value;
+  }
+
+  @JsonValue
+  public String getValue() {
+    return value;
+  }
+
+  @Override
+  public String toString() {
+    return String.valueOf(value);
+  }
+
+  public static ErrorCode fromValue(String text) {
+    for (ErrorCode b : ErrorCode.values()) {
+      if (String.valueOf(b.value).equals(text)) {
+        return b;
+      }
+    }
+    return null;
+  }
+}
